6d8a70af947ace17ae7c8ab3858e6d462b3e7309
husnajamil <55366890+husnajamil@users.noreply.github.com>
2019-11-14 13:20:46 +0800

    Update carloan.java

--- carloan.java
+++ carloan.java
@@ -1,74 +1,66 @@
-```java
 package com.company;
 
-import java.io.PrintStream;
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
-public class Main {
-
-    private static float InterestRate;
+public class testing {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(in);
+        Scanner sc = new Scanner(System.in);
 
-        float carPrice, downPayment, Principal = 0, balance, monthlyPayment = 0, interest, interestRate;
+        float carPrice, downPayment, Principal, balance, monthlyPayment, interest, interestRate;
         int loanPeriod, years=0;
 
-        out.println("ENTER THE CAR PRICE (RM)");
+        System.out.println("ENTER THE CAR PRICE (RM):");
         carPrice = sc.nextFloat();
 
         if (carPrice < 30000) {
-            out.println("VALUE CAR PRICE CANNOT LESS THAN RM30 000 ");
-           // carPrice = sc.nextFloat();
+            System.out.println("VALUE CAR PRICE CANNOT LESS THAN RM30 000 ");
         }
 
-        out.println("\nENTER THE DOWNPAYMENT(RM) : ");
+        System.out.println("\nENTER THE DOWNPAYMENT(RM) : ");
         downPayment = sc.nextFloat();
 
         if (downPayment >= carPrice) {
-            out.println("THE LOAN WAS PAID IN FULL");
+            System.out.println("THE LOAN WAS PAID IN FULL");
             downPayment = sc.nextFloat();
         }
 
-        out.println("\nENTER LOAN PERIOD(years) : ");
+        System.out.println("\nENTER LOAN PERIOD(years) : ");
         loanPeriod = sc.nextInt();
 
         if (loanPeriod < 5) {
-            out.println("THE LOAN PERIOD CANNOT LESS THAN 5 YEARS");
+            System.out.println("THE LOAN PERIOD CANNOT LESS THAN 5 YEARS");
         } else if (loanPeriod > 9) {
-            out.println("THE LOAN CANNOT BE LONGER THAN 9 YEARS");
+            System.out.println("THE LOAN CANNOT BE LONGER THAN 9 YEARS");
             loanPeriod = sc.nextInt();
-        } 
+        }
 
-        out.println("\nENTER INTEREST RATE(%) :");
+        System.out.println("\nENTER INTEREST RATE(%) :");
         interestRate = sc.nextInt();
 
         if (interestRate < 3) {
-            out.println("INTEREST RATE CANNOT LESS THAN 3%");
+            System.out.println("INTEREST RATE CANNOT LESS THAN 3%");
         } else if (interestRate > 7) {
-            out.println("INTEREST RATE CANNOT MORE THAN 7%");
+            System.out.println("INTEREST RATE CANNOT MORE THAN 7%");
         }
 
         float totalInterest = (carPrice - downPayment) * interestRate * loanPeriod;
         monthlyPayment = (carPrice - downPayment + totalInterest) / (loanPeriod * 12);
 
-        try (PrintStream monthlyPayment1 = out.printf("*\nYOUR MONTHLY REPAYMENT IS: RM'%.2f'\n", "monthlyPayment")) {
+        System.out.printf("YOUR MONTHLY REPAYMENT IS: RM%.2f\n", monthlyPayment);
+        System.out.printf("\t\t%s %17s%20s%20s", "years", "principal", "interest", "balance");
+
+
+        while (years <= loanPeriod){
+            Principal = monthlyPayment * 12 * years;
+            interest = (carPrice - downPayment) * (interestRate / 100) * years;
+            balance = (monthlyPayment * loanPeriod) * 12 - Principal;
+
+            System.out.printf("\n\t\t%d, %20.2f, %20.5f, %20.5f", years, Principal, interest, balance);
+            years++;
         }
 
-        PrintStream printf = out.printf("\t\t%d %s %s %s %d", new Object[]{"years", "principal", "interest", "balance"});
-
-
-        while (years <= loanPeriod)
-        Principal = monthlyPayment * 12 * years;
-        interest = (carPrice - downPayment) * (interestRate / 100) * years;
-        balance = (monthlyPayment * loanPeriod) * 12 - Principal;
-
-        System.out.printf("\n\t\t%7d, %f%,%f,%f", new Object[]{years, Principal, interest, balance});
-        years++;
     }
-
-    }
-    ```
+}
+ 
